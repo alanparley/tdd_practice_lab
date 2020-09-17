21fb6f3f48f76099b615c4073ce038a1b1e81545
Alan Parley <alanparley@hotmail.com>
2020-09-17 15:53:41 +0100

    first test

--- start_code/compound_interest/src/compound_interest.py
+++ start_code/compound_interest/src/compound_interest.py
@@ -1,2 +1,27 @@
 class CompoundInterest:
-    pass
+
+    def __init__(self, principal, rate, years):
+        self.principal = principal
+        self.rate = rate / 100
+        self.years = years
+        self.times_compounded = 12
+
+    def calculate(self):
+        amount = self.principal * \
+            (1 + (self.rate / self.times_compounded)
+             )**(self.years*self.times_compounded)
+        return round(amount, 2)
+
+# Write a program to compute the value of an investment compounded over time. The program should ask for the starting amount, the number of years to invest and the interest rate. Assume it is compounded 12 times a year.
+
+# The formula you’ll use for this is:
+
+# A = P(1 + r/n)nt
+
+# Where:
+
+# P is the principal amount
+# r is the annual rate of interest
+# t is the number of years the amount is invested
+# n is the number of times the interest is compounded per year
+# A is the amount at the end of the investment
--- start_code/compound_interest/tests/compound_interest_test.py
+++ start_code/compound_interest/tests/compound_interest_test.py
@@ -2,28 +2,29 @@ import unittest
 
 from src.compound_interest import CompoundInterest
 
+
 class CompoundInterestTest(unittest.TestCase):
 
-    # Tests
+    def test_first_test(self):
+        compound_interest = CompoundInterest(100, 10, 20)
+        self.assertEqual(732.81, compound_interest.calculate())
 
-    # Should return 732.81 given 100 principal, 10 percent, 20 years
+        # Should return 732.81 given 100 principal, 10 percent, 20 years
 
-    # Should return 181.94 given 100 principal, 6 percent, 10 years
+        # Should return 181.94 given 100 principal, 6 percent, 10 years
 
-    # Should return 149,058.55 given 100000 principal, 5 percent, 8 years
+        # Should return 149,058.55 given 100000 principal, 5 percent, 8 years
 
-    # Should return 0.00 given 0 principal, 10 percent, 1 year
+        # Should return 0.00 given 0 principal, 10 percent, 1 year
 
-    # Should return 100.00 given 100 principal, 0 percent, 10 years
+        # Should return 100.00 given 100 principal, 0 percent, 10 years
 
+        # Extention tests
 
-    # Extention tests
+        # Should return 118,380.16 given 100 principal, 5 percent, 8 years, 1000 per month
 
-    # Should return 118,380.16 given 100 principal, 5 percent, 8 years, 1000 per month
+        # Should return 156,093.99 given 100 principal, 5 percent, 10 years, 1000 per month
 
-    # Should return 156,093.99 given 100 principal, 5 percent, 10 years, 1000 per month
+        # Should return 475,442.59 given 116028.86, 7.5 percent, 8 years, 2006 per month
 
-    # Should return 475,442.59 given 116028.86, 7.5 percent, 8 years, 2006 per month
-
-    # Should return 718,335.96 given 116028.86 principal, 9 percent, 12 years, 1456 per month
-
+        # Should return 718,335.96 given 116028.86 principal, 9 percent, 12 years, 1456 per month
