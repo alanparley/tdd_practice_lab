be53d8600643b306de79766c3597760ad87eaf63
Alan Parley <alanparley@hotmail.com>
2020-09-17 12:53:21 +0100

    latest score test added and passed

--- start_code/high_scores/tests/high_scores_test.py
+++ start_code/high_scores/tests/high_scores_test.py
@@ -6,15 +6,19 @@ from src.high_scores import latest, personal_best, personal_top_three
 
 
 class HighScoresTest(unittest.TestCase):
-
-    # Tests
+    def setUp(self):
+        self.scores = [34, 54, 765, 234, 1, 32, 764, 98, 901, 378, 811]
 
     # Test latest score (the last thing in the list)
 
-    # Test personal best (the highest score in the list)
+    def test_latest(self):
+        self.assertEqual(811, latest(self.scores))
 
-    # Test top three from list of scores
+        # Test personal best (the highest score in the list)
 
+        # Test top three from list of scores
+    # def test_fizz_buzz__3_returns_fizz(self):
+    #     self.assertEqual("fizz", fizz_buzz(3))
     # Test ordered from highest tp lowest
 
     # Test top three when there is a tie
